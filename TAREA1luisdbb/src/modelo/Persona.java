68260c01a9f9b8b9a7df6c3addfdb826d4b64b73
luisdbb <luisdbb@educastur.org>
2024-11-27 19:06:10 +0100

    añadida la clase Persona.java en el package modelo

---
+++ TAREA1luisdbb/src/modelo/Persona.java
@@ -0,0 +1,28 @@
+package modelo;
+
+public class Persona {
+
+	private Long id;
+	private String nombre;
+	
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	@Override
+	public String toString() {
+		return "Persona [id=" + id + ", nombre=" + nombre + "]";
+	}
+	
+	
+	
+}
